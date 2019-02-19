34b0bc3d5806ac126c4f52388e2b3382ebe009b7
hl <827153647@qq.com>
2019-02-19 09:04:09 +0800

    架构

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+require.config({
+    baseUrl:"/js/",
+    paths:{
+        "swiper":"./libs/swiper.min",
+        "bscroll":"./libs/bscroll",
+        "flexible":"./libs/flexible",
+        "homePage":"./me/homePage"
+    }
+})
---
+++ src/js/page/index.js
@@ -0,0 +1,5 @@
+require(["./js/config.js"], function () {
+    require(["flexible","swiper","bscroll","homePage"], function (flexible,swiper,bscroll) {
+        console.log(swiper)
+    })
+})
